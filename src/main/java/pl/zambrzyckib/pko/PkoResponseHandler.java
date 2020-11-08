b68ef9fa9b23574feb053d337e4a3c5eac18d50f
Bartosz Zambrzycki <zambrzycki.bartosz@gmail.com>
2020-11-08 13:28:47 +0100

    Add javadoc to credential verification method

--- src/main/java/pl/zambrzyckib/pko/PkoResponseHandler.java
+++ src/main/java/pl/zambrzyckib/pko/PkoResponseHandler.java
@@ -8,6 +8,16 @@ import pl.zambrzyckib.exception.InvalidCredentialsException;
 
 public class PkoResponseHandler {
 
+  /**
+   * @param responseBody - response from POST request with user login, or password
+   * @param type - type of the requests from which the response comes from - POST with login, or
+   *                POST with password
+   *     This function checks whether the response contains fields that inform about
+   *     providing wrong credentials. When invalid credentials are provided, the server responds
+   *     with code 200 OK, but the response body contains nonempty field called errors, related to
+   *     specific object (fields.login or fields.password) depending on what request was sent
+   *     beforehand
+   */
   public void verifyCredentialsResponse(final String responseBody, final String type) {
     final var responseJson = new JSONObject(responseBody).getJSONObject("response");
     if (responseJson.has("fields")) {
