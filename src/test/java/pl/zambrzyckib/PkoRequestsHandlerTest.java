19b1827d5df506c02880568bf6a2cef10730de48
Bartosz Zambrzycki <zambrzycki.bartosz@gmail.com>
2020-11-09 13:27:12 +0100

    Add scrapper flow test

--- src/test/java/pl/zambrzyckib/PkoRequestsHandlerTest.java
+++ src/test/java/pl/zambrzyckib/PkoRequestsHandlerTest.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.util.Properties;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import pl.zambrzyckib.pko.PkoSession;
 import pl.zambrzyckib.pko.request.PkoRequestsHandler;
@@ -14,15 +15,20 @@ import pl.zambrzyckib.pko.response.body.LoginResponseBody;
 public class PkoRequestsHandlerTest {
 
   private final PkoRequestsHandler pkoRequestsHandler = new PkoRequestsHandler(new PkoSession());
+  private static final Properties properties = new Properties();
   private static final Gson GSON = new Gson();
+
+  @BeforeAll
+  @SneakyThrows
+  static void loadProperties() {
+    properties.load(new FileInputStream("src/test/resources/credentials.properties"));
+  }
 
   @Test
   @SneakyThrows
   void shouldReachPkoServerAndReceiveExpectedLoginResponse() {
-    final var properties = new Properties();
-    properties.load(new FileInputStream("src/test/resources/credentials.properties"));
     final String wrongLoginInput = "test" + "\n";
-    final String correctLoginInput = properties.getProperty("login");
+    final String correctLoginInput = properties.getProperty("login") + "\n";
     System.setIn(new ByteArrayInputStream((wrongLoginInput + correctLoginInput).getBytes()));
     final var wrongLoginResponse = pkoRequestsHandler.sendLoginRequest();
     final var correctLoginResponse = pkoRequestsHandler.sendLoginRequest();
---
+++ src/test/java/pl/zambrzyckib/PkoScrapperTest.java
@@ -0,0 +1,47 @@
+package pl.zambrzyckib;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.util.Properties;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import pl.zambrzyckib.exception.InvalidCredentialsException;
+import pl.zambrzyckib.pko.PkoScrapper;
+
+public class PkoScrapperTest {
+
+  private final PkoScrapper pkoScrapper = new PkoScrapper();
+  private static final Properties properties = new Properties();
+
+  @BeforeAll
+  @SneakyThrows
+  static void loadProperties() {
+    properties.load(new FileInputStream("src/test/resources/credentials.properties"));
+  }
+
+  @Test
+  void shouldNotThrowAnyExceptionWhenCredentialsAreCorrect() {
+    final String loginInput = properties.getProperty("login") + "\n";
+    final String passwordInput = properties.getProperty("password");
+    System.setIn(new ByteArrayInputStream((loginInput + passwordInput).getBytes()));
+    Assertions.assertDoesNotThrow(pkoScrapper::getAccountsInfo);
+  }
+
+  @Test
+  void shouldThrowInvalidCredentialsExceptionWhenProvidingWrongLogin() {
+    final String loginInput = "1" + "\n";
+    final String passwordInput = properties.getProperty("password");
+    System.setIn(new ByteArrayInputStream((loginInput + passwordInput).getBytes()));
+    Assertions.assertThrows(InvalidCredentialsException.class, pkoScrapper::getAccountsInfo);
+  }
+
+  @Test
+  void shouldThrowInvalidCredentialsExceptionWhenProvidingWrongPassword() {
+    final String loginInput = properties.getProperty("login") + "\n";
+    final String passwordInput = "test";
+    System.setIn(new ByteArrayInputStream((loginInput + passwordInput).getBytes()));
+    Assertions.assertThrows(InvalidCredentialsException.class, pkoScrapper::getAccountsInfo);
+  }
+}
