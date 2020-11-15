f7f2f2df8b15de84c66b8b5124686b4664e00911
Bartosz Zambrzycki <zambrzycki.bartosz@gmail.com>
2020-11-15 15:19:08 +0100

    Verify response right after sending it, remove PkoSessionTest

--- src/main/java/pl/zambrzyckib/pko/PkoScraper.java
+++ src/main/java/pl/zambrzyckib/pko/PkoScraper.java
@@ -35,20 +35,15 @@ public class PkoScraper {
 
   private void login(Credentials credentials) {
     Stream.of(pkoSession.sendLoginRequest(credentials.getLogin()))
-        .peek(ignored -> System.out.println("Login sent"))
-        .peek(pkoResponsesHandler::verifyLoginResponse)
         .peek(this::saveSessionId)
         .map(
-            response -> pkoSession.sendPasswordRequest(response, credentials.getPassword()))
-        .peek(ignored -> System.out.println("Password sent"))
-        .peek(pkoResponsesHandler::verifyPasswordResponse)
-        .peek(ignored -> System.out.println("Logged in successfully"));
+            response -> pkoSession.sendPasswordRequest(response, credentials.getPassword()));
   }
 
   private List<AccountSummary> fetchAccountsInfo() {
     return Stream.of(pkoSession.sendAccountsInfoRequest())
         .map(pkoResponsesHandler::getAccountSummaries)
-        .peek(ignored -> System.out.println("Fetched accounts data"))
+        .peek(ignored -> System.out.println("Successfully fetched accounts data"))
         .get();
   }
 
--- src/main/java/pl/zambrzyckib/pko/PkoSession.java
+++ src/main/java/pl/zambrzyckib/pko/PkoSession.java
@@ -2,39 +2,48 @@ package pl.zambrzyckib.pko;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import io.vavr.collection.Stream;
 import lombok.Getter;
 import lombok.Setter;
 import pl.zambrzyckib.connection.BankConnection;
 import pl.zambrzyckib.connection.JsoupConnection;
 import pl.zambrzyckib.connection.Response;
 import pl.zambrzyckib.pko.request.PkoRequests;
+import pl.zambrzyckib.pko.response.PkoResponsesHandler;
 
 public class PkoSession {
 
-  public static final String HOME_URL = "https://www.ipko.pl/";
-  public static final String LOGIN_ENDPOINT = "ipko3/login";
-  public static final String ACCOUNT_INFO_ENDPOINT = "ipko3/init";
+    public static final String HOME_URL = "https://www.ipko.pl/";
+    public static final String LOGIN_ENDPOINT = "ipko3/login";
+    public static final String ACCOUNT_INFO_ENDPOINT = "ipko3/init";
 
-  private final BankConnection bankConnection;
+    private final PkoResponsesHandler pkoResponsesHandler = new PkoResponsesHandler();
+    private final BankConnection bankConnection;
 
-  @Setter
-  private String sessionId;
+    @Setter
+    private String sessionId;
 
-  public PkoSession() {
-    this.bankConnection = new JsoupConnection(HOME_URL, true);
-  }
+    public PkoSession() {
+        this.bankConnection = new JsoupConnection(HOME_URL, true);
+    }
 
-  public Response sendLoginRequest(String login) {
-    return bankConnection
-            .send(PkoRequests.userLoginPostRequest(login));
-  }
+    public Response sendLoginRequest(String login) {
+        return Stream.of(bankConnection
+                .send(PkoRequests.userLoginPostRequest(login)))
+                .peek(pkoResponsesHandler::verifyLoginResponse)
+                .get();
+    }
 
-  public Response sendPasswordRequest(Response sendLoginResponse, String password) {
-    return bankConnection
-            .send(PkoRequests.userPasswordPostRequest(password, sessionId, sendLoginResponse));
-  }
+    public Response sendPasswordRequest(Response sendLoginResponse, String password) {
+        return Stream.of(bankConnection
+                .send(PkoRequests.userPasswordPostRequest(password, sessionId, sendLoginResponse)))
+                .peek(pkoResponsesHandler::verifyPasswordResponse)
+                .get();
+    }
 
-  public Response sendAccountsInfoRequest() {
-    return bankConnection.send(PkoRequests.accountsInfoPostRequest(sessionId));
-  }
+    public Response sendAccountsInfoRequest() {
+        return bankConnection.send(PkoRequests.accountsInfoPostRequest(sessionId));
+    }
+
 }
