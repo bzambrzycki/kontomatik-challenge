bde377cea6f0e0f9bb9c65bef259738542d089d6
Bartosz Zambrzycki <zambrzycki.bartosz@gmail.com>
2020-11-16 14:29:17 +0100

    Add login, fetch and display flow test

--- src/test/java/pl/zambrzyckib/integration/PkoScraperTest.java
+++ src/test/java/pl/zambrzyckib/integration/PkoScraperTest.java
@@ -2,8 +2,11 @@ package pl.zambrzyckib.integration;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static pl.zambrzyckib.integration.PkoIntegrationTestSpec.properties;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -45,5 +48,17 @@ public class PkoScraperTest {
         assertThrows(
                 InvalidCredentialsException.class,
                 () -> pkoScraper.getAccountSummaries(Credentials.of(login, wrongPassword)));
+    }
+
+    @Test
+    void shouldLoginToBankAndDisplayAccountsSummary(){
+        final var standardOut = System.out;
+        final var byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        final var login = properties.getProperty("login");
+        final var password = properties.getProperty("password");
+        pkoScraper.getAndDisplayAccountsInfo(Credentials.of(login, password));
+        assertTrue(byteArrayOutputStream.toString().contains("Successfully fetched accounts info"));
+        System.setOut(standardOut);
     }
 }
