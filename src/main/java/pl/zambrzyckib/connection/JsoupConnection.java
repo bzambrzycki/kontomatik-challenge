43bcea56788e07a1b1a72d472892979f334e3e87
Bartosz Zambrzycki <bartosz.zambrzycki@kontomatik.com>
2020-12-10 16:19:50 +0100

    Make flow of JsoupConnection send method procedural

--- src/main/java/pl/zambrzyckib/connection/JsoupConnection.java
+++ src/main/java/pl/zambrzyckib/connection/JsoupConnection.java
@@ -1,6 +1,6 @@
 package pl.zambrzyckib.connection;
 
-import io.vavr.control.Try;
+import lombok.SneakyThrows;
 import org.jsoup.Connection;
 import org.jsoup.Connection.Method;
 import org.jsoup.Jsoup;
@@ -17,22 +17,19 @@ public class JsoupConnection implements HttpAgent {
   }
 
   @Override
+  @SneakyThrows
   public Response send(Request request) {
     final Map<String, String> headers = request.headers != null ? request.headers : Map.of();
     final Map<String, String> cookies = request.cookies != null ? request.cookies : Map.of();
-    return Try.of(
-            () ->
-                connection
-                    .url(request.baseUrl + request.endpoint)
-                    .requestBody(request.body)
-                    .method(Method.valueOf(request.method.toString()))
-                    .headers(headers)
-                    .cookies(cookies)
-                    .execute())
-        .map(
-            response ->
-                Response.of(
-                    response.body(), response.statusCode(), response.headers(), response.cookies()))
-        .get();
+    final Connection.Response jsoupResponse =
+        connection
+            .url(request.baseUrl + request.endpoint)
+            .requestBody(request.body)
+            .method(Method.valueOf(request.method.toString()))
+            .headers(headers)
+            .cookies(cookies)
+            .execute();
+    return Response.of(
+        jsoupResponse.body(), jsoupResponse.statusCode(), jsoupResponse.headers(), jsoupResponse.cookies());
   }
 }
