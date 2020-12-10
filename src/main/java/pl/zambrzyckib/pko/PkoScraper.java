9355903efa1ef7478cac5f2bd0f854dbdafdb8c9
Bartosz Zambrzycki <bartosz.zambrzycki@kontomatik.com>
2020-12-10 15:17:14 +0100

    Make flow of PkoScraper methods procedural

--- src/main/java/pl/zambrzyckib/pko/PkoScraper.java
+++ src/main/java/pl/zambrzyckib/pko/PkoScraper.java
@@ -2,13 +2,11 @@ package pl.zambrzyckib.pko;
 
 import com.google.gson.Gson;
 import io.vavr.collection.List;
-import io.vavr.collection.Stream;
 import pl.zambrzyckib.UserInterface;
 import pl.zambrzyckib.connection.Response;
 import pl.zambrzyckib.model.AccountSummary;
 import pl.zambrzyckib.model.Credentials;
 import pl.zambrzyckib.pko.response.PkoResponseParser;
-
 
 public class PkoScraper {
 
@@ -27,16 +25,18 @@ public class PkoScraper {
   }
 
   private List<AccountSummary> getAccountSummaries(Credentials credentials) {
-    return Stream.of(fetchAccountsInfo(credentials))
-        .peek(ignored -> userInterface.displaySuccessMessage())
-        .map(PkoResponseParser::getAccountSummariesFromResponse)
-        .get();
+    final Response accountsResponse = fetchAccountsInfo(credentials);
+    userInterface.displaySuccessMessage();
+    return PkoResponseParser.getAccountSummariesFromResponse(accountsResponse);
   }
 
   private Response fetchAccountsInfo(Credentials credentials) {
-    return Stream.of(pkoSession.sendLoginRequest(credentials.login))
-        .map(response -> pkoSession.sendPasswordRequest(response, credentials.password))
-        .map(response -> pkoSession.fetchAccounts())
-        .get();
+    login(credentials);
+    return pkoSession.fetchAccounts();
+  }
+
+  private void login(Credentials credentials) {
+    final Response loginResponse = pkoSession.sendLoginRequest(credentials.login);
+    pkoSession.sendPasswordRequest(loginResponse, credentials.password);
   }
 }
